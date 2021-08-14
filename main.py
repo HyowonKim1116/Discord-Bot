55ebdbdf0e8d62826509150d02daa53d838a358f
Hyowon Kim <87758380+HyowonKim1116@users.noreply.github.com>
2021-08-15 03:21:50 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import os
+
+def main():
+    prefix = '!'
+    intents = discord.Intents.all()
+
+    client = commands.Bot(command_prefix = prefix, intents = intents)
+
+    for filename in os.listdir('./cogs'):
+        if '.py' in filename:
+            filename = filename.replace('.py', '')
+            client.load_extension(f"cogs.{filename}")
+
+    with open('token.txt', 'r') as f:
+        client.run(f.read())
+
+if __name__ == '__main__':
+    main()
