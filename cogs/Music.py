7611d0d14fb212c307ccd1f9455a2c8810ed249e
Hyowon Kim <87758380+HyowonKim1116@users.noreply.github.com>
2021-08-29 09:23:32 +0900

    Update Music.py

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 from os.path import abspath
 from youtube_dl import YoutubeDL
-from .module.youtube import getUrl, getInfo, getTime
+from .module.youtube import *
 import discord
 
 class Music(commands.Cog): #클래스 Music 선언, commands.Cog 상속
@@ -18,7 +18,7 @@ class Music(commands.Cog): #클래스 Music 선언, commands.Cog 상속
         print('Music Cog is Ready')
     
     #음악 재생 명령어
-    @commands.command(name = '음악재생')
+    @commands.command(name = '음악재생', description = '키워드를 유튜브에서 검색해 음악을 재생합니다.')
     async def play_music(self, ctx, *, keyword):
         if ctx.voice_client is None:
             if ctx.author.voice:
@@ -27,7 +27,7 @@ class Music(commands.Cog): #클래스 Music 선언, commands.Cog 상속
                 embed = discord.Embed(
                     title = '오류 발생',
                     description = '음성 채널에 들어간 후 명령어를 사용해주세요!',
-                    color = discord.Color.red()
+                    color = discord.Color.dark_red()
                 )
                 await ctx.send(embed = embed)
                 raise commands.CommandError('Author is not connected to a voice channel.')
@@ -37,13 +37,13 @@ class Music(commands.Cog): #클래스 Music 선언, commands.Cog 상속
         #키워드를 검색했을 때 음악이 존재하는 경우
         try:
             data = self.DL.extract_info(getUrl(keyword), download = False)
-            await ctx.send(embed = getInfo(data))
+            await ctx.send(embed = getEmbed(data))
         #키워드를 검색했을 때 음악이 존재하지 않는 경우
         except:
             embed = discord.Embed(
                 title = '오류 발생',
                 description = '해당 키워드에 대한 음악이 검색되지 않았습니다.',
-                color = discord.Color.red()
+                color = discord.Color.dark_red()
             )
             await ctx.send(embed = embed)
             raise commands.CommandError('No music was found for these keywords.')
@@ -72,7 +72,7 @@ class Music(commands.Cog): #클래스 Music 선언, commands.Cog 상속
         await ctx.send(embed = embed)
     
     #음악 종료 명령어
-    @commands.command(name = '음악종료')
+    @commands.command(name = '음악종료', description = '현재 재생 중인 음악을 종료합니다.')
     async def quit_music(self, ctx):
         voice = ctx.voice_client
         if voice.is_connected():
@@ -80,31 +80,29 @@ class Music(commands.Cog): #클래스 Music 선언, commands.Cog 상속
             embed = discord.Embed(
                 title = '음악 종료',
                 description = '음악 재생을 종료합니다.',
-                color = discord.Color.blue()
+                color = discord.Color.red()
             )
             await ctx.send(embed = embed)
     
     #음악 일시정지 명령어
-    @commands.command(name = '일시정지')
+    @commands.command(name = '일시정지', description = '현재 재생 중인 음악을 일시정지합니다.')
     async def pause_music(self, ctx):
         voice = ctx.voice_client
         if voice.is_playing():
             voice.pause()
             embed = discord.Embed(
-                title = '',
                 description = '음악 재생을 일시정지합니다.',
                 color = discord.Color.purple()
             )
             await ctx.send(embed = embed)
     
     #음악 재개 명령어
-    @commands.command(name = '다시시작')
+    @commands.command(name = '다시시작', description = '멈춘 부분부터 음악을 재생합니다.')
     async def resume_music(self, ctx):
         voice = ctx.voice_client
         if voice.is_paused():
             voice.resume()
             embed = discord.Embed(
-                title = '',
                 description = '멈춘 부분부터 음악을 재생합니다.',
                 color = discord.Color.purple()
             )
